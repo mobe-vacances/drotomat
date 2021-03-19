edb19799ccd4adc3298d85965dc713aed2e75e35
Nicolas ANDRÉ <45142835+iomega11@users.noreply.github.com>
2021-03-19 14:43:58 +0100

    Added border in solid platform

--- app/src/main/java/m2dl/mobe/vacances/challenge/game/platform/SolidPlatform.java
+++ app/src/main/java/m2dl/mobe/vacances/challenge/game/platform/SolidPlatform.java
@@ -10,12 +10,18 @@ public class SolidPlatform extends Platform {
 
     public SolidPlatform(int left, int top, int right, int bottom, Player player) {
         super(left, top, right, bottom, player);
+        borderPaint = new Paint();
     }
 
     @Override
     public void initPaints() {
         fillPaint.setStyle(Paint.Style.FILL);
         fillPaint.setColor(Color.DKGRAY);
+
+        borderPaint.reset();
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(5);
+        borderPaint.setColor(Color.DKGRAY);
     }
 
 }
