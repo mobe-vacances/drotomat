0013e6666041dca7358a24f3c5940ab6d50ef9b3
Nils Tancrède Rodriguez <nils.rodriguez@gmv.com>
2021-03-19 09:54:34 +0100

    plateforme + debut xml parser

--- app/src/main/java/m2dl/mobe/vacances/challenge/game/GameActivity.java
+++ app/src/main/java/m2dl/mobe/vacances/challenge/game/GameActivity.java
@@ -1,24 +1,43 @@
 package m2dl.mobe.vacances.challenge.game;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import java.util.Random;
 
-import m2dl.mobe.vacances.challenge.R;
+import m2dl.mobe.vacances.challenge.game.background.Background;
 import m2dl.mobe.vacances.challenge.game.mobengine.activities.MobeGameActivity;
-import m2dl.mobe.vacances.challenge.game.mobengine.activities.SoundActivity;
+import m2dl.mobe.vacances.challenge.game.mobengine.core.GameEngine;
 import m2dl.mobe.vacances.challenge.game_over.GameOverActivity;
+import m2dl.mobe.vacances.challenge.level.PlateformeDure;
+import m2dl.mobe.vacances.challenge.level.aPlateforme;
+import m2dl.mobe.vacances.challenge.utils.XMLParser;
 
 public class GameActivity extends MobeGameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game); // À ENLEVER QUAND IL Y AURA UN VRAI JEU
+
+        Background background = new Background();
+
+
+        GameEngine.reset();
+        GameEngine.addGameElements(
+                background
+        );
+        readLevel();
+        GameEngine.start();
     }
+
+
+    private void readLevel(){
+        XMLParser xmlParser = new XMLParser();
+        System.out.println(xmlParser.read(this));
+
+    }
+
 
     public void loose(View v) { // À ENLEVER QUAND IL Y AURA UN VRAI JEU
         Intent intent = new Intent(GameActivity.this, GameOverActivity.class);
