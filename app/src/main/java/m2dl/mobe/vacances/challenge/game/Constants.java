2a06d5a440d3d864c8a6188f1702ca847fa42d82
Kevin Delcourt <kevin.delcourt@master-developpement-logiciel.fr>
2021-03-19 09:17:47 +0100

    Added player + jump

--- app/src/main/java/m2dl/mobe/vacances/challenge/game/Constants.java
+++ app/src/main/java/m2dl/mobe/vacances/challenge/game/Constants.java
@@ -14,4 +14,16 @@ public class Constants {
 
 
     public static final float VOLUME_MENU_MUSIC = 0.9f;
+
+
+    public static final int PLAYER_WIDTH = 50;
+    public static final int PLAYER_HEIGHT = 100;
+    public static final float PLAYER_MAX_X_SPEED = 0.4f;
+    public static final float PLAYER_MAX_Y_SPEED = 0.9f;
+    public static final float PLAYER_JUMP_ACCELERATION = -0.25f;
+    public static final float PLAYER_GRAVITY = 0.025f;
+    public static final float PLAYER_Y_INERTIA = 0.0003f;
+    public static final float PLAYER_X_INERTIA = 0.0003f;
+
+    public static final int Z_INDEX_PLAYER = 100;
 }
--- app/src/main/java/m2dl/mobe/vacances/challenge/game/GameActivity.java
+++ app/src/main/java/m2dl/mobe/vacances/challenge/game/GameActivity.java
@@ -10,6 +10,8 @@ import java.util.Random;
 import m2dl.mobe.vacances.challenge.R;
 import m2dl.mobe.vacances.challenge.game.mobengine.activities.MobeGameActivity;
 import m2dl.mobe.vacances.challenge.game.mobengine.activities.SoundActivity;
+import m2dl.mobe.vacances.challenge.game.mobengine.core.GameEngine;
+import m2dl.mobe.vacances.challenge.game.player.Player;
 import m2dl.mobe.vacances.challenge.game_over.GameOverActivity;
 
 public class GameActivity extends MobeGameActivity {
@@ -17,10 +19,14 @@ public class GameActivity extends MobeGameActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game); // À ENLEVER QUAND IL Y AURA UN VRAI JEU
+
+        GameEngine.addGameElements(
+                new Player(0,0)
+        );
+
     }
 
-    public void loose(View v) { // À ENLEVER QUAND IL Y AURA UN VRAI JEU
+    public void loose(View v) {
         Intent intent = new Intent(GameActivity.this, GameOverActivity.class);
         intent.putExtra("score", new Random().nextInt(10000));
         startActivity(intent);
